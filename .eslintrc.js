f5091c20e8121b45a2744fc05d68777a4bc86d79
Erik Thorelli <ethorelli@gmail.com>
2020-07-20 21:26:24 -0400

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,12 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,31 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@@screens': './src/screens',
+          '@@navigation': './src/navigation',
+          '@@components': './src/components',
+          '@@utils': './src/utils',
+          '@@state': './src/state',
+          '@@assets': './src/assets',
+          '@@cards': './src/assets/cards',
+        },
+      },
+    ],
+  ],
 };
